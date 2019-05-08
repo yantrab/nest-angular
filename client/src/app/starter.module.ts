59a883de215e039c88594f5c8b901c8c283aff04
shulich <shulich321@gmail.com>
2019-05-08 11:19:47 +0300

    marge

--- client/src/app/starter.module.ts
+++ client/src/app/starter.module.ts
@@ -21,21 +21,21 @@ export class AppComponent {
 }
 
 const routes: Routes = [
-  { path: '', redirectTo: 'praedicta', pathMatch: 'full' },
-  // { path: '', redirectTo: 'login/app1', pathMatch: 'full' },
-  // { path: 'app1', loadChildren: 'src/app/app1/app1.module#App1Module' },
-  {
-    path: 'app1', loadChildren: 'src/app/app1/app1.module#App1Module', canActivate: [Guard],
-    data: { app: App.mf}
-  },
-  { path: 'praedicta', loadChildren: 'src/app/praedicta-site/praedicta-site.module#PraedictaSiteModule' },
-  
-  { path: 'login/:site', loadChildren: 'src/app/auth/auth.module#AuthModule' },
+    { path: '', redirectTo: 'praedicta', pathMatch: 'full' },
+    // { path: '', redirectTo: 'login/app1', pathMatch: 'full' },
+    // { path: 'app1', loadChildren: 'src/app/app1/app1.module#App1Module' },
+    {
+        path: 'app1', loadChildren: 'src/app/app1/app1.module#App1Module', canActivate: [Guard],
+        data: { app: App.mf }
+    },
+    { path: 'praedicta', loadChildren: 'src/app/praedicta-site/praedicta-site.module#PraedictaSiteModule' },
 
-  { path: 'intercom', loadChildren: 'src/app/intercom/intercom.module#IntercomModule' },
-  { path: 'macro', loadChildren: 'src/app/macro/macro.module#MacroModule', canActivate: [Guard], data: { app: App.macro}},
+    { path: 'login/:site', loadChildren: 'src/app/auth/auth.module#AuthModule' },
 
-  { path: '**', redirectTo: 'praedicta' } 
+    { path: 'intercom', loadChildren: 'src/app/intercom/intercom.module#IntercomModule' },
+    { path: 'macro', loadChildren: 'src/app/macro/macro.module#MacroModule', canActivate: [Guard], data: { app: App.macro } },
+
+    { path: '**', redirectTo: 'praedicta' },
 
     { path: '**', redirectTo: 'login/mf' },
 ];
@@ -60,4 +60,4 @@ const routes: Routes = [
         },
     ],
 })
-export class AppModule {}
+export class AppModule { }
