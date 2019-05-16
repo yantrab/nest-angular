fea62488d537653fef0367f1a92cf507e0903ef3
Yaniv Trabelsi <yaniv@praedicta.com>
2019-05-16 10:08:18 +0300

    fig gourd bug

--- shared/guard.ts
+++ shared/guard.ts
@@ -2,4 +2,5 @@ import { User, App, Role, Permission } from './models/user.model';
 export const hasPermission = (user: User, app: App) =>
     user && user.roles && user.roles.some(role => role.app === App.admin || role.app === app);
 export const hasRole = (user: User, app: App, permissions: Permission[]) =>
+    user &&
     user.roles.some(role => role.app === App.admin || (role.app === app && permissions.includes(role.permission)));
