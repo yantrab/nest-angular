fb49554939e619c9b32e9f5c263df41eecc38c8b
shulich <shulich321@gmail.com>
2019-05-16 13:08:34 +0300

    Changes in Hebrew...

--- client/src/app/praedicta-site/praedicta-site/praedicta-site.component.ts
+++ client/src/app/praedicta-site/praedicta-site/praedicta-site.component.ts
@@ -1,31 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { I18nService } from 'src/app/shared/services/i18n.service';
-import { LoginRequest } from 'shared';
 import { I18nRootObject } from 'src/api/i18n/site.i18n';
 
 @Component({
-  selector: 'p-praedicta-site',
-  templateUrl: './praedicta-site.component.html',
-  styleUrls: ['./praedicta-site.component.scss']
+    selector: 'p-praedicta-site',
+    templateUrl: './praedicta-site.component.html',
+    styleUrls: ['./praedicta-site.component.scss'],
 })
 export class PraedictaSiteComponent implements OnInit {
-  direction = 'rtl';
-  dic: I18nRootObject;
-  constructor(
-    public i18nService: I18nService) {
-    this.i18nService.dic.subscribe(result => {
-
-      this.dic = result as any;
+    direction = 'rtl';
+    dic: I18nRootObject;
+    isHide: boolean = true;
+    constructor(public i18nService: I18nService) {
+        this.i18nService.dic.subscribe(result => {
+            this.dic = result as any;
+        });
     }
-    );
+    ngOnInit() {}
 
-  }
-  ngOnInit() {
-  }
-  changeDirection() {
-// tslint:disable-next-line: triple-equals
-    this.i18nService.language = 'en';
-    this.direction = this.direction === 'ltr' ? 'rtl' : 'ltr';
-  }
-
+    changeDirection() {
+        this.i18nService.language = this.i18nService.language == 'he' ? 'en' : 'he';
+    }
 }
