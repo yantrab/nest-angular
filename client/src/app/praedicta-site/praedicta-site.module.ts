f6c29335abc8e0cf2b17a93d1fdaf8620633d7cb
shulich <shulich321@gmail.com>
2019-05-22 11:13:42 +0300

    system

--- client/src/app/praedicta-site/praedicta-site.module.ts
+++ client/src/app/praedicta-site/praedicta-site.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PraedictaSiteComponent } from './praedicta-site/praedicta-site.component';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { I18nService } from '../shared/services/i18n.service';
+import { SystemComponent } from './system/system.component';
+import { SystemsManagerComponent } from './systems-manager/systems-manager.component';
+import { ContentComponent } from './content/content.component';
+@Component({
+    selector: 'p-site',
+    template: '<router-outlet></router-outlet>',
+    styleUrls: ['./site.component.scss'],
+})
+export class SiteComponent {}
 
 @NgModule({
-    declarations: [PraedictaSiteComponent],
+    declarations: [SiteComponent, PraedictaSiteComponent, SystemComponent, SystemsManagerComponent, ContentComponent],
     providers: [I18nService, { provide: 'baseUrlI18n', useValue: '../../assets/i18n/site' }],
     imports: [
         CommonModule,
@@ -14,12 +23,17 @@ import { I18nService } from '../shared/services/i18n.service';
         RouterModule.forChild([
             {
                 path: '',
-                component: PraedictaSiteComponent,
-                // children: [
-                //   { path: '', redirectTo: 'dumy' },
-                //   { path: 'poly', component: PolyComponent },
-                //   { path: 'dumy', component: DumyComponent },
-                // ]
+                component: SiteComponent,
+                children: [
+                    {
+                        path: '',
+                        component: PraedictaSiteComponent,
+                    },
+                    {
+                        path: ':site',
+                        component: SystemsManagerComponent,
+                    },
+                ],
             },
         ]),
     ],
--- client/src/app/praedicta-site/praedicta-site/praedicta-site.component.ts
+++ client/src/app/praedicta-site/praedicta-site/praedicta-site.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { I18nService } from 'src/app/shared/services/i18n.service';
 import { I18nRootObject } from 'src/api/i18n/site.i18n';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'p-praedicta-site',
@@ -11,12 +12,15 @@ export class PraedictaSiteComponent implements OnInit {
     direction = 'rtl';
     dic: I18nRootObject;
     isHide: boolean = true;
-    constructor(public i18nService: I18nService) {
+    constructor(public i18nService: I18nService, private router: Router) {
         this.i18nService.dic.subscribe(result => {
             this.dic = result as any;
         });
     }
     ngOnInit() {}
+    navigate(to: string) {
+        this.router.navigate([window.location.pathname + '/' + to, {}]).then();
+    }
 
     changeDirection() {
         this.i18nService.language = this.i18nService.language == 'he' ? 'en' : 'he';
