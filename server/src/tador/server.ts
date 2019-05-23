748689f0064347d35ac4b7bd4d7aed8db783f85a
yantrab <yantrab@gmail.com>
2019-05-23 08:46:39 +0300

    tador

--- server/src/tador/server.ts
+++ server/src/tador/server.ts
@@ -1,4 +1,4 @@
-import {createServer} from 'net';
+import { createServer } from 'net';
 const port = 3000;
 const host = '127.0.0.1';
 
@@ -8,28 +8,41 @@ server.listen(port, host, () => {
 });
 server.on('connection', sock => {
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-    sock.on('testNumber', data => {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        const result = data.replace('test', '').trim();
-        console.log('testNumber:' + data + ':' + result);
-        sock.emit('testNumber', result);
+    sock.on('data', msg => {
+        const msgString = msg.toString('utf8');
+        console.log('DATA ' + sock.remoteAddress + ': ' + msgString);
+        const action = +msgString[0];
+        const data = msgString.slice(1);
+        let result;
+        switch (action) {
+            case 1: {
+                result = data.replace('test', '').trim();
+                sock.write(result);
+                break;
+            }
+            case 2: {
+                result = data
+                    .split('')
+                    .map(n => {
+                        let num = +n + 1;
+                        if (num === 10) {
+                            num = 0;
+                        }
+                        return num;
+                    })
+                    .join('');
+
+                break;
+            }
+
+            default:
+                break;
+        }
+
+        console.log(data + ':' + result);
+        sock.write(result);
     });
 
-
-    sock.on('seq', data => {
-        const result = data.split('').map(n => {
-            let num = +n + 1;
-            if (num === 10) {
-                num = 0;
-            }
-            return num;
-        })
-        .join('');
-        console.log('seq:' + data + ':' + result);
-        sock.emit('seq', result);
-    })
-
-    // Add a 'close' event handler to this instance of socket
     sock.on('close', data => {
         console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
     });
