0f8700ea021fd2fe049b64a31a6ddd39695ba94e
shulich <shulich321@gmail.com>
2019-05-30 14:10:46 +0300

    arrow

---
+++ client/src/app/praedicta-site/image/image.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { I18nService } from '../../shared/services/i18n.service';
+import { I18nRootObject } from '../../../api/i18n/site.i18n';
+
+@Component({
+    selector: 'p-image',
+    templateUrl: './image.component.html',
+    styleUrls: ['./image.component.scss'],
+})
+export class ImageComponent implements OnInit {
+    dic: I18nRootObject;
+    imageUrl: string;
+    @Input() currentSystem: string;
+    constructor(public i18nService: I18nService) {
+        this.i18nService.dic.subscribe(result => {
+            this.dic = result as any;
+        });
+    }
+    ngOnInit() {
+        this.imageUrl = `assets/img/systems/${this.currentSystem}.PNG`;
+    }
+}
