d5fc6d703601544f9a976f30294bcc881aa75dfd
yannivt <yanivt@traxretail.com>
2020-05-15 11:28:49 +0300

    serve static files with fastify

--- serve-static.ts
+++ serve-static.ts
@@ -7,7 +7,7 @@ fastify.register(require('fastify-static'), {
 
 const start = async () => {
     try {
-        await fastify.listen(4200)
+        await fastify.listen(4200,'0.0.0.0')
         fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.error(err)
