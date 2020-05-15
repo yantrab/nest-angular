2f0ef0d0dc9774d846f3815ff0653013d1345e0c
yannivt <yanivt@traxretail.com>
2020-05-15 13:03:31 +0300

    serve static files with fastify

--- serve-static.ts
+++ serve-static.ts
@@ -1,13 +1,36 @@
+import * as fs from 'fs';
+import { extname } from 'path';
 const fastify = require('fastify')({ logger: true })
-const path = require('path')
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm',
+};
 
-fastify.register(require('fastify-static'), {
-    root: path.join(__dirname, 'client/dist'),
+fastify.get('*', (_, reply) => {
+    const filePath = extname(_.raw.url) ? './client/dist' + _.raw.url : './client/dist/index.html';
+    const ext = extname(filePath).toLowerCase();
+    const contentType = mimeTypes[ext] || 'application/octet-stream';
+    const stream = fs.createReadStream(filePath)
+    reply.type(contentType).send(stream)
 })
 
+// Run the server!
 const start = async () => {
     try {
-        await fastify.listen(4200,'0.0.0.0')
+        await fastify.listen(4200)
         fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.error(err)
