87553436acb457eda5297f5ff22f4640ef849a7a
yannivt <yanivt@traxretail.com>
2020-05-15 11:13:17 +0300

    serve static files with fastify

--- serve-static.ts
+++ serve-static.ts
@@ -1,38 +1,18 @@
-import { createServer } from 'http';
-import { readFile } from 'fs';
-import { extname } from 'path';
-const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.wasm': 'application/wasm',
-};
-createServer((request, response) => {
-    const filePath = extname(request.url) ? './client/dist' + request.url : './client/dist/index.html';
-    const ext = extname(filePath).toLowerCase();
-    const contentType = mimeTypes[ext] || 'application/octet-stream';
-    readFile(filePath, (error, content) => {
-        if (error) {
-            if (error.code === 'ENOENT') {
-                console.log(filePath);
-            } else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-            }
-        } else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-}).listen(4200);
+const fastify = require('fastify')()
+const path = require('path')
+
+fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, 'client/dist'),
+})
+
+const start = async () => {
+    try {
+        await fastify.listen(4200)
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
